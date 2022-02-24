7fc7415f4939c1a607342e923407632fad6a6093
Brian <78610140+baxm1@users.noreply.github.com>
2022-02-24 15:39:39 -0600

    Update FRAM_functions.ino

--- FRAM_functions/FRAM_functions.ino
+++ FRAM_functions/FRAM_functions.ino
@@ -1,18 +1,27 @@
+// this file contains the functions used to read and write frames of data to the FRAM
 
 #include "Adafruit_FRAM_I2C.h"
 #include <Wire.h>
 
-Adafruit_FRAM_I2C fram     = Adafruit_FRAM_I2C();
+// declare the FRAM
+Adafruit_FRAM_I2C fram = Adafruit_FRAM_I2C();
 
-// Global variables
+// declare global variables
+// this is where sensor pins will go
 uint16_t addressCounter = 0;
-int imuPin = A0;
-int temp_sense = A0;
+uint8_t syncMSB = 160;
+uint8_t syncLSB = 161;
+int tempPin = A0;
 
+
+// FUNCTION: write_frame
+//
+// DESCRIPTION: writes a frame of sensor data (stored in buffer[] array) to the address given by the value of addressCounter in the FRAM chip.
+//              the frame is formatted as: a sync byte of 160, a sync byte of 161, then the sensor data.
+// 
+// INPUTS: num_sensors - the number of sensors that data has been read from
 
 void write_frame(int num_sensors){
-  uint8_t syncMSB = 160;
-  uint8_t syncLSB = 161;
   fram.write(addressCounter, syncMSB);
   addressCounter ++;
   fram.write(addressCounter, syncLSB);
@@ -34,9 +43,8 @@ void clearMem(){
   
 }
 
-
 void takeReading(){
-  uint16_t sensVal = analogRead(temp_sense);
+  uint16_t sensVal = analogRead(tempPin);
   uint8_t sensLSB = lowByte(sensVal);
   uint8_t sensMSB = highByte(sensVal);
   memcpy(buffer[0], (void *) &sensMSB, 1);
@@ -58,13 +66,6 @@ void read_frame(){
   }
   
 }
-
-
-void loadTo(sd card){
-  
-
-}
-
 
 void setup() {
   // put your setup code here, to run once:
